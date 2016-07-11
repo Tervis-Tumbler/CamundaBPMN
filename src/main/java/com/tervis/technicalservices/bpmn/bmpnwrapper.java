a42f57621f78a89dec0c85541277d34889a6047d
ChrisMagnuson <hourglasssand@gmail.com>
2016-07-11 10:18:07 -0400

    Initial commit

---
+++ src/main/java/com/tervis/technicalservices/bpmn/bmpnwrapper.java
@@ -0,0 +1,8 @@
+package com.tervis.technicalservices.bpmn;
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Loan Approval App")
+public class bmpnwrapper extends ServletProcessApplication {
+
+}
